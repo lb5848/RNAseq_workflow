2204980a1b8a8165f5f438e21f0775c2c5fbcf13
Luca Biavati <lucabiavati@Lucas-MacBook-Pro.local>
2020-12-02 19:20:47 -0500

    try and identify some genes

--- DESeq2_full_workflow_LB.R
+++ DESeq2_full_workflow_LB.R
@@ -209,10 +209,10 @@ dds <- DESeq(dds, test = "Wald")
 resultsNames(dds)
 
 res <- results_all(dds, vs = "all", trt = "group")
-plot_volcano(res[[1]])
+plot_volcano(res[[2]])
 res_sig <- res %>% lapply(function(x) {
   x <- x %>% as_tibble() %>% filter(padj < p.adj.cutoff) %>% filter(abs(log2FoldChange) > log2FC.cutoff)})
-plot_volcano(res_sig[[6]])
+plot_volcano(res_sig[[2]])
 res_sig
 # save csv files - significant genes only - all contrasts
 lapply(1:length(res_sig), function(i){
@@ -226,9 +226,10 @@ res_sighciR <- res_sig %>% lapply(function(x){
 
 names(res_sighciR)
 # BM_Norm vs PBL_Norm
-x <- top_counts(res_sighciR[[1]], vsd, top = 1000, filter = TRUE, sort_fc = TRUE)
+res_sighciR$`BM_Norm vs. PBL_Norm`
+x <- top_counts(res_sighciR[[1]], vsd, top = 1500, filter = TRUE, sort_fc = TRUE)
 
-plot_genes(x, intgroup = "group", scale = "row", show_rownames = FALSE, annotation_names_col = FALSE, 
+plot_genes(x, intgroup = "group", scale = "diff", show_rownames = FALSE, annotation_names_col = FALSE, 
            show_colnames = TRUE)
 out <- plot_genes(x, intgroup = "group", scale = "row", show_rownames = FALSE, annotation_names_col = FALSE, 
                   show_colnames = TRUE)
@@ -239,23 +240,35 @@ out.clust.2 <- out.clust %>% filter(out.clust$cluster == 2) %>% select(-cluster)
 plot.clust <- x %>% filter(id %in% out.clust.2$id)
 plot_genes(plot.clust, intgroup = "group", scale = "row", show_rownames = FALSE, 
            annotation_names_col = FALSE, show_colnames = FALSE, output = "pheatmap")
+# cluster 2 -> BM up genes
 
 #BM_Norm vs BM_Hyp
-x <- top_counts(res_sighciR[[2]], vsd, top = 150, filter = TRUE, sort_fc = TRUE)
+res_sighciR$`BM_Norm vs. BM_Hyp`
+
+x <- top_counts(res_sighciR[[2]], vsd, top = 300, filter = TRUE, sort_fc = TRUE)
 noMS <- x %>% select(-grep("MS", colnames(x)))
-plot_genes(x, intgroup = "group", scale = "row", show_rownames = FALSE, annotation_names_col = FALSE, 
+plot_genes(x, intgroup = "group", scale = "diff", show_rownames = FALSE, annotation_names_col = FALSE, 
            show_colnames = FALSE)
 plot_genes(noMS, intgroup = "group", scale = "row", show_rownames = FALSE, annotation_names_col = FALSE, 
            show_colnames = FALSE)
 out <- plot_genes(noMS, intgroup = "group", scale = "row", show_rownames = FALSE, annotation_names_col = FALSE, 
            show_colnames = FALSE)
-out.clust <- cbind(noMS, cluster = sort(cutree(out$tree_row, k = 6)))
+out.clust <- cbind(noMS, cluster = sort(cutree(out$tree_row, k = 2)))
 out.clust <- as_tibble(out.clust)
 out.clust.2 <- out.clust %>% filter(out.clust$cluster == 1) %>% select(-cluster)
 plot.clust <- noMS %>% filter(id %in% out.clust.2$id)
 plot_genes(plot.clust, intgroup = "group", scale = "row", show_rownames = TRUE, 
            annotation_names_col = FALSE, show_colnames = FALSE, output = "pheatmap")
-
+# cluster 1 -> Hyp up genes
+plotHyp <- plot_genes(plot.clust, intgroup = "group", scale = "row", show_rownames = TRUE, 
+                      annotation_names_col = FALSE, show_colnames = FALSE, output = "pheatmap")
+plot(plotHyp$tree_row)
+out.Hyp <- cbind(plot.clust, cluster = sort(cutree(plotHyp$tree_row, k = 2)))
+out.Hyp <- as_tibble(out.Hyp)
+out.Hyp.2 <- out.Hyp %>% filter(cluster == 2) %>% select(-cluster)
+plot.Hyp.specific <- plot.clust %>% filter(id %in% out.Hyp.2$id)
+plot_genes(plot.Hyp.specific, intgroup = "group", scale = "row", show_rownames = TRUE, 
+           annotation_names_col = FALSE, show_colnames = FALSE, output = "pheatmap")
 # PBL_Norm vs BM_Hyp
 x <- top_counts(res_sighciR[[4]], vsd, top = 2800, filter = TRUE, sort_fc = FALSE)
 
